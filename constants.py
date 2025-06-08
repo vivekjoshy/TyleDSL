8bd39abe0d5007d6b1b6e01596cb5a6c1d46b445
Vivek Joshy <git@vivekjoshy.com>
2025-06-08 23:28:44 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,35 @@
+from opinion.tyle.types import IntegerPoint
+
+F: bool = False
+T: bool = True
+
+ZERO: int = 0
+ONE: int = 1
+TWO: int = 2
+THREE: int = 3
+FOUR: int = 4
+FIVE: int = 5
+SIX: int = 6
+SEVEN: int = 7
+EIGHT: int = 8
+NINE: int = 9
+TEN: int = 10
+
+NEG_ONE: int = -1
+NEG_TWO: int = -2
+
+DOWN: IntegerPoint = (1, 0)
+RIGHT: IntegerPoint = (0, 1)
+UP: IntegerPoint = (-1, 0)
+LEFT: IntegerPoint = (0, -1)
+
+ORIGIN: IntegerPoint = (0, 0)
+UNITY: IntegerPoint = (1, 1)
+NEG_UNITY: IntegerPoint = (-1, -1)
+UP_RIGHT: IntegerPoint = (-1, 1)
+DOWN_LEFT: IntegerPoint = (1, -1)
+
+ZERO_BY_TWO: IntegerPoint = (0, 2)
+TWO_BY_ZERO: IntegerPoint = (2, 0)
+TWO_BY_TWO: IntegerPoint = (2, 2)
+THREE_BY_THREE: IntegerPoint = (3, 3)
